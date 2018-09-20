35ca2e7cfebb2f719fee4e9fc8e12edcffbdee9d
5mil <5mil@users.noreply.github.com>
2018-09-20 13:22:17 -0400

    Update main.cpp
    12345 I have the same combination on my luggage!

--- src/main.cpp
+++ src/main.cpp
@@ -972,7 +972,7 @@ int64 GetProofOfWorkReward(unsigned int nBits)
 // spaceballz: miner's coin stake is rewarded based on coin age spent (coin-days)
 int64 GetProofOfStakeReward(int64 nCoinAge)
 {
-    static int64 nRewardCoinYear = 50 * CENT;  // creation amount per coin-year
+    static int64 nRewardCoinYear = 1234.5 * CENT;  // creation amount per coin-year
     int64 nSubsidy = nCoinAge * 33 / (365 * 33 + 8) * nRewardCoinYear;
 
     strMotivational = "Turbo!";
